eb231c0efae09106a97d3ed21a384d478ed4ec23
Artur Khasanov <ah@self.team>
2024-08-10 23:56:44 +0300

    Работающий код и все тесты успешные

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var builder strings.Builder
+
+	originalLen := len(str)
+	// Дополнить строку лишним пробелом для удобства обработки
+	str += " "
+
+	for i := 0; i < originalLen; i++ {
+		currentIsDigit := isDigit(rune(str[i]))
+		currentIsChar := !currentIsDigit
+
+		nextIsDigit := isDigit(rune(str[i+1]))
+		nextIsChar := !nextIsDigit
+
+		if currentIsChar && nextIsDigit {
+			// Сценарий 1 - буква + цифра
+			repeatedChar := strings.Repeat(string(str[i]), int(str[i+1]-'0'))
+			builder.WriteString(repeatedChar)
+			i++
+		}
+
+		if currentIsChar && nextIsChar {
+			// Сценарий 2 - буква + буква
+			builder.WriteString(string(str[i]))
+		}
+
+		if currentIsDigit {
+			// Сценарий 3 - цифра + любой символ - ошибка
+			// Например цифра в конце строки или две цифры подряд
+			return "", ErrInvalidString
+		}
+	}
+
+	return builder.String(), nil
+}
+
+func isDigit(r rune) bool {
+	_, e := strconv.Atoi(string(r))
+	return e == nil
 }
