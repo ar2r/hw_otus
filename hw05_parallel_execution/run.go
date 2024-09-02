4b4585e8167abaa5c5cda8661e0117b2a32891b1
Artur Khasanov <ah@self.team>
2024-09-02 19:14:14 +0300

    Базовая реализация

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,67 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var wg sync.WaitGroup
+	var errCount int
+	tasksCh := make(chan Task)    // Канал для задач
+	errCh := make(chan error, m)  // Канал для ошибок
+	stopCh := make(chan struct{}) // Канал для сигнала остановки
+
+	// Запустить n горутин
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			//  Чтение каналов с задачами и сигналом остановки
+			for {
+				select {
+				case task, ok := <-tasksCh:
+					if !ok {
+						// 🤬 Канал задач закрыт
+						return
+					}
+					if err := task(); err != nil {
+						// 🚚 Отправить ошибку в канал ошибок
+						errCh <- err
+					}
+				case <-stopCh:
+					// 🛑 Получен сигнал остановки
+					return
+				}
+			}
+		}()
+	}
+
+	// Отправить задачи в канал
+	go func() {
+		for _, task := range tasks {
+			select {
+			case tasksCh <- task:
+			case <-stopCh:
+				// 🔴 Прекратить отправку задач, если получен сигнал остановки
+				return
+			}
+		}
+		close(tasksCh)
+	}()
+
+	// Ожидание завершения всех горутин
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
+
+	// Ожидание m ошибок
+	for err := range errCh {
+		if err != nil {
+			errCount++
+			if errCount >= m {
+				close(stopCh)
+				return ErrErrorsLimitExceeded
+			}
+		}
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -18,14 +19,17 @@ func TestRun(t *testing.T) {
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
-
 		var runTasksCount int32
+
+		var mu sync.Mutex
 
 		for i := 0; i < tasksCount; i++ {
 			err := fmt.Errorf("error from task %d", i)
 			tasks = append(tasks, func() error {
+				mu.Lock()
 				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
 				atomic.AddInt32(&runTasksCount, 1)
+				mu.Unlock()
 				return err
 			})
 		}
@@ -35,13 +39,14 @@ func TestRun(t *testing.T) {
 		err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		mu.Lock()
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+		mu.Unlock()
 	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
-
 		var runTasksCount int32
 		var sumTime time.Duration
 
@@ -64,7 +69,10 @@ func TestRun(t *testing.T) {
 		elapsedTime := time.Since(start)
 		require.NoError(t, err)
 
-		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.Eventually(t, func() bool {
+			return runTasksCount == int32(tasksCount)
+		}, sumTime/2, time.Millisecond*10, "not all tasks were completed")
+
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
 }
