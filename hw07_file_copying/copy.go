a2aadb819a8bf7cb0e784a1c470b4f8d08ef7b5f
Artur Khasanov <ah@self.team>
2024-09-21 17:57:57 +0300

    Добавил прогресс бар

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"os"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	maxFileSizeWithoutProgressBar int64 = 1024
+	ErrUnsupportedFile                  = errors.New("unsupported file")
+	ErrOffsetExceedsFileSize            = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
@@ -44,7 +46,15 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	if err != nil {
 		return err
 	}
-	defer toFile.Close()
+	defer func() {
+		err := toFile.Close()
+		if err != nil {
+			// IDE говорит, что нужно эту ошибку обработать.
+			// Но в данном случае, если ошибка возникнет, то это будет критическая ошибка.
+			// Тут нужно выкинуть панику или этого достаточно??
+			fmt.Println("Error closing file:", err)
+		}
+	}()
 
 	// Устанавливаем смещение в исходном файле
 	if _, err = fromFile.Seek(offset, io.SeekStart); err != nil {
@@ -56,10 +66,58 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 		limit = fileInfo.Size() - offset
 	}
 
-	_, err = io.CopyN(toFile, fromFile, limit)
-	if err != nil && errors.Is(err, io.EOF) {
+	// Копируем данные без прогресс бара:
+	// - если размер копируемых данных меньше maxFileSizeWithoutProgressBar
+	if limit < maxFileSizeWithoutProgressBar {
+		_, err = io.CopyN(toFile, fromFile, limit)
+		if err != nil && errors.Is(err, io.EOF) {
+			return err
+		}
+		return nil
+	}
+
+	// Иначе: Копирование с визуализацией процесса копирования
+	if err := copyWithProgres(fromFile, toFile, limit); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func copyWithProgres(fromFile *os.File, toFile *os.File, limit int64) error {
+	buf := make([]byte, 1024)
+	var copied int64
+
+	for copied < limit {
+		bytesToRead := int64(len(buf))
+		if limit-copied < bytesToRead {
+			bytesToRead = limit - copied
+		}
+
+		n, err := fromFile.Read(buf[:bytesToRead])
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		if n == 0 {
+			break
+		}
+
+		if _, err := toFile.Write(buf[:n]); err != nil {
+			return err
+		}
+
+		copied += int64(n)
+		printProgress(copied, limit)
+	}
+	return nil
+}
+
+func printProgress(copied, total int64) {
+	percent := float64(copied) / float64(total) * 100
+	fmt.Printf("\rProgress: %.2f%% (%d/%d bytes)", percent, copied, total)
+	if copied == total {
+		fmt.Println("\nCopy complete")
+	}
+	// time.Sleep(100 * time.Millisecond) // для плавного обновления прогресса
 }
