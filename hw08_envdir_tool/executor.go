36b64d40d5db702993f20d23f1b628503a916b3e
Artur Khasanov <ah@self.team>
2024-09-29 16:47:53 +0300

    Executor

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,38 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	execCmd := exec.Command(cmd[0], cmd[1:]...)
+	execCmd.Stdout = os.Stdout
+	execCmd.Stderr = os.Stderr
+
+	// Передать текущие переменные окружения
+	execCmd.Env = append(execCmd.Env, os.Environ()...)
+
+	// Передать переменные окружения из файлов
+	for k, v := range env {
+		if v.NeedRemove {
+			execCmd.Env = append(execCmd.Env, k+"=")
+			continue
+		}
+		execCmd.Env = append(execCmd.Env, k+"="+v.Value)
+	}
+
+	// Запустить команду
+	errCmd := execCmd.Run()
+	if errCmd != nil {
+		var exitError *exec.ExitError
+		if errors.As(errCmd, &exitError) {
+			// Вернуть exit code выполненной команды
+			return exitError.ExitCode()
+		}
+	}
+
+	return 0
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,61 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	tests := []struct {
+		name     string
+		cmd      []string
+		env      Environment
+		expected int
+		stdout   string
+	}{
+		{
+			name:     "Простой echo",
+			cmd:      []string{"echo", "hello"},
+			env:      Environment{},
+			expected: 0,
+			stdout:   "hello\n",
+		},
+		{
+			name:     "Использование аргумента",
+			cmd:      []string{"sh", "-c", "echo hello"},
+			env:      Environment{},
+			expected: 0,
+			stdout:   "hello\n",
+		},
+		{
+			name:     "Использование ENV",
+			cmd:      []string{"sh", "-c", "echo hello $NAME!"},
+			env:      Environment{"NAME": {"world", false}},
+			expected: 0,
+			stdout:   "hello world!\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var stdout bytes.Buffer
+			r, w, _ := os.Pipe()
+			oldStdout := os.Stdout
+			defer func() { os.Stdout = oldStdout }()
+			os.Stdout = w
+
+			got := RunCmd(tt.cmd, tt.env)
+			w.Close()
+			io.Copy(&stdout, r)
+
+			if got != tt.expected {
+				t.Errorf("RunCmd() = %v, want %v", got, tt.expected)
+			}
+			if stdout.String() != tt.stdout {
+				t.Errorf("stdout = %v, want %v", stdout.String(), tt.stdout)
+			}
+		})
+	}
 }
