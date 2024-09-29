987e093847e2cee878da5d623bbbaa89bf173c33
Artur Khasanov <ah@self.team>
2024-09-29 16:55:07 +0300

    Более читабальный код

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -4,14 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 )
 
 var (
 	ErrNotEnoughArguments = errors.New("not enough arguments")
 	ErrReadingEnvDir      = errors.New("error reading env dir")
-	ErrRunCommand         = errors.New("error running command")
 	ErrGettingAbsPath     = errors.New("error getting absolute path")
 	ErrGettingEnvDirPath  = errors.New("error getting env dir path")
 )
@@ -20,46 +18,9 @@ func main() {
 	if len(os.Args) < 3 {
 		printExit(ErrNotEnoughArguments, nil)
 	}
-	envDirPath := getEnvDirPath()
-	executableFilePath := getExecutableFilePath()
-	myEnvironment := getEnvironment(envDirPath)
-	osArgs := getOsArgs()
-
-	errCmd := runCommand(executableFilePath, myEnvironment, osArgs)
-
-	if errCmd != nil {
-		if exitError, ok := errCmd.(*exec.ExitError); ok {
-			// Вернуть exit code выполненной команды
-			os.Exit(exitError.ExitCode())
-		} else {
-			printExit(ErrRunCommand, errCmd)
-		}
-	}
-}
-
-func runCommand(executableFilePath string, myEnvironment Environment, osArgs []string) error {
-	// Запустить команду
-	cmd := exec.Command(executableFilePath, osArgs...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	// Передать текущие переменные окружения
-	for _, v := range os.Environ() {
-		cmd.Env = append(cmd.Env, v)
-	}
-
-	// Передать переменные окружения из файлов
-	for k, v := range myEnvironment {
-		if v.NeedRemove {
-			cmd.Env = append(cmd.Env, k+"=")
-			continue
-		}
-		cmd.Env = append(cmd.Env, k+"="+v.Value)
-	}
-
-	// Запустить команду
-	errCmd := cmd.Run()
-	return errCmd
+	cmd := append([]string{getExecutableFilePath()}, getOsArgs()...)
+	exitCode := RunCmd(cmd, getEnvironment(getEnvDirPath()))
+	os.Exit(exitCode)
 }
 
 func getOsArgs() []string {
